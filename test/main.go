c824d0081a3d27895fdc8891a45756f86d831c0b
p_caiwfeng <p_caiwfeng@tencent.com>
2021-04-08 19:11:37 +0800

    new feature add test case

---
+++ test/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"errors"
+	"nocalhost/test/nhctl"
+)
+
+func main() {
+	commitId := nhctl.GetCommitId()
+	if commitId == "" {
+		panic(errors.New("this is should not happen"))
+	}
+	nhctl.InstallNhctl(commitId)
+	go nhctl.Init()
+	if i := <-nhctl.StatusChan; i != 1 {
+		nhctl.StopChan <- 1
+	}
+	nhctl.InstallBookInfo()
+	nhctl.PortForward()
+	module := "details"
+	nhctl.Dev(module)
+	nhctl.Sync(module)
+	nhctl.End(module)
+}
